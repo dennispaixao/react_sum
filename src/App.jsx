470b98b6aa9df844b272ac1b983c5e3ad7ecc483
Dennis Paixão <dennispaixaoads@gmail.com>
2025-05-15 12:49:01 -0300

    solution 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.scss';
 import { Sum } from './components/Sum/Sum';
+
 export const App = () => (
   <>
-    <p><Sum a={2} b={3} /></p>
-    <p><Sum a={-5} b={5} /></p>
-    <p><Sum b={10} /></p>
-    <p><Sum  b={5} /></p>
-    <p><Sum  /></p>
+    <p>
+      <Sum a={2} b={3} />
+    </p>
+    <p>
+      <Sum a={-5} b={5} />
+    </p>
+    <p>
+      <Sum b={10} />
+    </p>
+    <p>
+      <Sum b={5} />
+    </p>
+    <p>
+      <Sum />
+    </p>
   </>
 );
--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,3 +1,4 @@
 export const Sum = ({ a = 0, b = 0 }) => {
-  return <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>;
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  return <>{`Sum of ${a} and ${b} is ${a + b}`}</>;
 };
